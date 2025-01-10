81fc34c199c254ea7f4e75483f2e892429322a2e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-01-10 10:38:02 +0530

    Refactor Dice class to use recursive method for rolling multiple dice

--- 1. Snake and Ladder/src/Dice.java
+++ 1. Snake and Ladder/src/Dice.java
@@ -1,4 +1,6 @@
 
+import java.util.Random;
+
 public class Dice {
 
     private int diceCount;
@@ -7,10 +9,15 @@ public class Dice {
         this.diceCount = diceCount;
     }
 
+    private int getRandomDiceValue(int diceCount) {
+        if (diceCount == 0) {
+            return 0;   // Base case
+        }
+        int randomDiceValue = new Random().nextInt(6) + 1;
+        return randomDiceValue + getRandomDiceValue(diceCount - 1);     // Recursive call
+    }
+
     public int rollDice() {
-        int maxValue = (6 * diceCount) - (1 * diceCount);
-        double randomValue = Math.random();
-        int diceResult = (int) (randomValue * maxValue) + 1;
-        return diceResult;
+        return getRandomDiceValue(diceCount);
     }
 }
